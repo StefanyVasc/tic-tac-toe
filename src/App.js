82de36fd71f552b476669b6cdd3126f94d3e1e99
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-06-22 19:00:33 -0300

    :construction: Challenge unique 047 done
    Included Sobre

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from "react";
-import "./App.css";
-import logo from "./logo.svg";
+
+const sobreStyle = {
+  width: "50px",
+  height: "17px",
+  fontFamily: "MuseoModerno",
+  fontSize: "14px",
+  fontWeight: "bold",
+  textTransform: "uppercase",
+  color: "#707070",
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendendo ReactJS
-        </a>
-      </header>
-    </div>
-  );
+  return <h1 style={sobreStyle}>sobre</h1>;
 }
 
 export default App;
