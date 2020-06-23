8a1ea6a285997e111054c69629f9ce2efc387006
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-06-22 21:23:17 -0300

    :recycle: fix challenge 047
    fix link "sobre"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
-
-const sobreStyle = {
-  width: "50px",
-  height: "17px",
-  fontFamily: "MuseoModerno",
-  fontSize: "14px",
-  fontWeight: "bold",
-  textTransform: "uppercase",
-  color: "#707070",
-};
+import "./App.css";
 
 function App() {
-  return <h1 style={sobreStyle}>sobre</h1>;
+  return (
+    <a href="#" className="action">
+      sobre
+    </a>
+  );
 }
 
 export default App;
