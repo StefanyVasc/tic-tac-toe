28952010e8821748cf72a532ca30b53f2c393de8
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-06-23 00:46:00 -0300

    :construction: Challenge advanced 048 done
    included folder settings and files for colors, sizes and spacings basic style settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./App.css";
+import "./elements/base.css";
 import "./generic/reset.css";
+import "./settings/colors.css";
+import "./settings/sizes.css";
+import "./settings/spacings.css";
 
 function App() {
   return (
