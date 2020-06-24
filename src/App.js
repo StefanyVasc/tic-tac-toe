5e88d6c3552258705c079c902a6a2cae86480329
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-06-24 19:38:47 -0300

    :construction: Challenge basic 049 done
    created object "Logo"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import About from "./objects/About/index.js";
+import Logo from "./objects/Logo/index.js";
 
 function App() {
-  return <About />;
+  return (
+    <div>
+      <Logo />
+      <About />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/objects/Logo/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import "./styles.css";
+
+const Logo = () => {
+  return <img src={logo} alt="logo do collabcode" className="logo" />;
+};
+
+export default Logo;
