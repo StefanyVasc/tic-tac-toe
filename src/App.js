132d11513c3abc1f15efad96162ef723579db50a
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-06-24 20:23:25 -0300

    :construction: challenge intermediate 049 done
    created object "Menu"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "./objects/About/index.js";
 import Logo from "./objects/Logo/index.js";
+import Menu from "./objects/Menu/index.js";
 
 function App() {
   return (
     <div>
       <Logo />
       <About />
+      <Menu />
     </div>
   );
 }
---
+++ src/objects/Menu/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const Menu = () => {
+  return (
+    <div>
+      <span></span>
+    </div>
+  );
+};
+
+export default Menu;
