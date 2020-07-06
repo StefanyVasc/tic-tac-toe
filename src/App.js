de5edaeb583cd6f23e30305057b508bf3f40ef4a
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-07-06 16:26:54 -0300

    :construction: challenge basic 052 done
    created object "Layer"

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./App.css";
 import Hash from "./components/Hash";
 import Header from "./components/Header";
 import Checkbox from "./objects/Checkbox";
+import Layer from "./objects/Layer";
 
 const App = () => {
   return (
-    <main className="app">
-      <Header />
-      <Hash />
-      <Checkbox id="show" value="show" content="Mostrar eventos" />
-    </main>
+    <>
+      <main className="app">
+        <Header />
+        <Hash />
+        <Checkbox id="show" value="show" content="Mostrar eventos" />
+        <Layer />
+      </main>
+    </>
   );
 };
 
---
+++ src/objects/Layer/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./styles.css";
+
+const Layer = () => <div className="layer"></div>;
+
+export default Layer;
