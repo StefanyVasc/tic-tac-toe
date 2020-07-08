1995ea1ee763e489c30e391cb39c2cf530a87c98
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-07-07 21:17:17 -0300

    :recycle: included refactoring "HeaderGame"  on Layer

--- src/components/Layer/index.js
+++ src/components/Layer/index.js
@@ -3,12 +3,12 @@ import Avatar from "../../objects/Avatar";
 import Description from "../../objects/Description";
 import SocialBar from "../../objects/SocialBar";
 import Title from "../../objects/Title";
-import Header from "../Header";
+import HeaderGame from "../HeaderGame";
 import "./styles.css";
 
 const Layer = () => (
   <div className="layer">
-    <Header logo="light" />
+    <HeaderGame logo="light" />
     <Avatar />
     <Title content="Stefany Sá" />
     <Description content="Student of information systems at UFRPE. Trainee at CESAR. Has knowledge in programming languages Python and Java. Currently passionate about JavaScript." />
