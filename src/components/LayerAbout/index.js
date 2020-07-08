dd802723008d43900b67b32b10007408f20cd660
StefanyVasc <stefany.vasc.sa@gmail.com>
2020-07-07 22:10:18 -0300

    :recycle: refactoring "Avatar"

--- src/components/LayerAbout/index.js
+++ src/components/LayerAbout/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Avatar from "../../objects/Avatar";
+import AvatarProfile from "../../objects/AvatarProfile";
 import Description from "../../objects/Description";
 import SocialBar from "../../objects/SocialBar";
 import Title from "../../objects/Title";
@@ -9,7 +9,7 @@ import "./styles.css";
 const LayerAbout = () => (
   <div className="layer-about">
     <HeaderGame logo="light" />
-    <Avatar />
+    <AvatarProfile />
     <Title content="Stefany Sá" />
     <Description content="Student of information systems at UFRPE. Trainee at CESAR. Has knowledge in programming languages Python and Java. Currently passionate about JavaScript." />
     <SocialBar />
--- src/objects/Avatar/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import perfil from "../../img/perfil-pixelart.png";
-import "./styles.css";
-
-const Avatar = () => (
-  <img className="avatar" alt="avatar of tic tac toe creator" src={perfil} />
-);
-
-export default Avatar;
